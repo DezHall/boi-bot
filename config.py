c0fdf5fc593f972cfca6cece25313ebfa4921ea6
Desmond <dezhall97@yahoo.com>
2018-02-01 12:00:30 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-#config file used to declare login information created from Reddit
-
-username = "DrDezzz"
-password = "Crosby97"
-client_id = "CsUWA32EYXxiRg"
-client_secret = "F-x1OFiOYNQJTLHh0vSlSzxlY9s"
